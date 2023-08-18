33ade968669f8240b2af0f4bc0ab3b6047771630
Raboro <marius.woerfelreal@gmail.com>
2023-08-18 21:36:12 +0200

    removed comments

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,6 @@ export default class BirthdayTrackerPlugin extends Plugin {
 		});
 		ribbonIconEl.addClass('birthday-tracker-plugin-ribbon-class');
 
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
 			id: 'open-sample-modal-simple',
 			name: 'Open sample modal (simple)',
@@ -27,7 +26,7 @@ export default class BirthdayTrackerPlugin extends Plugin {
 				new Notice("worked");
 			}
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
+
 		this.addCommand({
 			id: 'sample-editor-command',
 			name: 'Sample editor command',
@@ -37,16 +36,12 @@ export default class BirthdayTrackerPlugin extends Plugin {
 			}
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new BirthdayTrackerSettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
 		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
 			console.log('click', evt);
 		});
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
 		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
