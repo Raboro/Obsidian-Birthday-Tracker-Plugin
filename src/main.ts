88ddb4d1643675cf7b3907238ab3be1af3fc0abe
Raboro <marius.woerfelreal@gmail.com>
2023-08-18 22:19:55 +0200

    extracted settings into settings.ts

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
-import { App, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Notice, Plugin } from 'obsidian';
+import { BirthdayTrackerSettingTab } from './settings';
 
 interface BirthdayTrackerSettings {
 	dateFormatting: string;
@@ -46,58 +47,5 @@ export default class BirthdayTrackerPlugin extends Plugin {
 
 	async saveSettings() {
 		await this.saveData(this.settings);
-	}
-}
-
-class BirthdayTrackerSettingTab extends PluginSettingTab {
-	plugin: BirthdayTrackerPlugin;
-
-	constructor(app: App, plugin: BirthdayTrackerPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Date formatting')
-			.setDesc('Format your dates will be displayed and collected')
-			.addText(text => text
-				.setPlaceholder('Enter your format')
-				.setValue(this.plugin.settings.dateFormatting)
-				.onChange(async (value) => {
-					let noticeMessage = "Wrong date formatting!!";
-					if (this.isFormattingValid(value)) {
-						this.plugin.settings.dateFormatting = value;
-						await this.plugin.saveSettings();
-						noticeMessage = "Valid date formatting";
-					} 
-					new Notice(noticeMessage);
-				}));
-	}
-
-	isFormattingValid(format: string): boolean {
-		const containsDoubleD: boolean = this.formatContains("DD", format);
-		const containsDoubleM: boolean = this.formatContains("MM", format);
-		const containsFourY: boolean = this.formatContains("YYYY", format);
-		return containsDoubleD && containsDoubleM && containsFourY && !this.containsInvalidChars(format);
-	}
-
-	formatContains(subStr: string, format: string): boolean {
-		return format.contains(subStr) || format.contains(subStr.toLowerCase());
-	}
-
-	containsInvalidChars(format: string): boolean {
-		const invalidChars: string[] = ["A", "B", "C", "E", "F", "G", "H", "I", "J", "K", "L", "N", 
-										"O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Z"];
-		for (const invalidChar in invalidChars) {
-			if (this.formatContains(invalidChar, format)) {
-				return true;
-			}
-		}
-		return false;
 	}
 }
---
+++ src/settings.ts
@@ -0,0 +1,55 @@
+import { App, Notice, PluginSettingTab, Setting } from "obsidian";
+import BirthdayTrackerPlugin from "./main";
+
+export class BirthdayTrackerSettingTab extends PluginSettingTab {
+	plugin: BirthdayTrackerPlugin;
+
+	constructor(app: App, plugin: BirthdayTrackerPlugin) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+	display(): void {
+		const {containerEl} = this;
+
+		containerEl.empty();
+
+		new Setting(containerEl)
+			.setName('Date formatting')
+			.setDesc('Format your dates will be displayed and collected')
+			.addText(text => text
+				.setPlaceholder('Enter your format')
+				.setValue(this.plugin.settings.dateFormatting)
+				.onChange(async (value) => {
+					let noticeMessage = "Wrong date formatting!!";
+					if (this.isFormattingValid(value)) {
+						this.plugin.settings.dateFormatting = value;
+						await this.plugin.saveSettings();
+						noticeMessage = "Valid date formatting";
+					} 
+					new Notice(noticeMessage);
+				}));
+	}
+
+	isFormattingValid(format: string): boolean {
+		const containsDoubleD: boolean = this.formatContains("DD", format);
+		const containsDoubleM: boolean = this.formatContains("MM", format);
+		const containsFourY: boolean = this.formatContains("YYYY", format);
+		return containsDoubleD && containsDoubleM && containsFourY && !this.containsInvalidChars(format);
+	}
+
+	formatContains(subStr: string, format: string): boolean {
+		return format.contains(subStr) || format.contains(subStr.toLowerCase());
+	}
+
+	containsInvalidChars(format: string): boolean {
+		const invalidChars: string[] = ["A", "B", "C", "E", "F", "G", "H", "I", "J", "K", "L", "N", 
+										"O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Z"];
+		for (const invalidChar in invalidChars) {
+			if (this.formatContains(invalidChar, format)) {
+				return true;
+			}
+		}
+		return false;
+	}
+}
