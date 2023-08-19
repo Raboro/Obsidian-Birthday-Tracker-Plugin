66709184cac51b354be9ad30dfdd74185e11b760
Raboro <marius.woerfelreal@gmail.com>
2023-08-19 20:40:36 +0200

    converted date as string to date

--- src/birthday.ts
+++ src/birthday.ts
@@ -1,7 +1,23 @@
 export default class Birthday {
 	str: string
+    date: Date
 
 	constructor(str: string, dateFormatting: string) {
 		this.str = str
+        this.convertStringToDate(dateFormatting);
 	}
+
+    private convertStringToDate(dateFormatting: string) {
+        const dayIndex = dateFormatting.search("DD");
+        const monthIndex = dateFormatting.search("MM");
+        const yearIndex = dateFormatting.search("YYYY");
+        this.date = new Date();
+        this.date.setDate(this.dateNumber(dayIndex, dayIndex+2));
+        this.date.setMonth(this.dateNumber(monthIndex, monthIndex+2, 1)); // month has one offset
+        this.date.setFullYear(this.dateNumber(yearIndex, yearIndex+4));
+    }
+
+    private dateNumber(start: number, end: number, offset?: number): number {
+        return parseInt(this.str.substring(start, end)) - (offset ?? 0)
+    }
 }
