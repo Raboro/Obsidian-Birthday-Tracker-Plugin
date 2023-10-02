b2fe70dcab4e89678ada95cd3b18df6dae1b0a5f
Raboro <marius.woerfelreal@gmail.com>
2023-10-02 17:27:19 +0200

    added year container with styles

--- src/views/yearOverviewView.ts
+++ src/views/yearOverviewView.ts
@@ -16,5 +16,9 @@ export class YearOverviewView extends ItemView {
     async onOpen() {
         const { contentEl } = this;
         contentEl.createEl('h1', {text: 'Birthday Tracker - Year Overview'});
+        const container = contentEl.createDiv({cls: 'yearContainer'});
+        for(let i = 0; i < 12; i++) {
+            container.createDiv({text:""});
+        }
     }
 }
