2f8581f40621428ffdcd08ca2f6abbd7e4c62608
Raboro <marius.woerfelreal@gmail.com>
2023-10-02 17:41:26 +0200

    added monthName

--- src/views/yearOverviewView.ts
+++ src/views/yearOverviewView.ts
@@ -2,6 +2,8 @@ import { ItemView } from "obsidian";
 
 export const BIRTHDAY_TRACKER_YEAR_OVERVIEW_VIEW_TYPE = "Birthday-Tracker-Year-Overview";
 
+const MONTHS = ["January", "February", "March", "April", "Mai", "June", "July", "August", "September", "October", "November", "December"]
+
 export class YearOverviewView extends ItemView {
     icon = "calendar-days";
 
@@ -17,8 +19,9 @@ export class YearOverviewView extends ItemView {
         const { contentEl } = this;
         contentEl.createEl('h1', {text: 'Birthday Tracker - Year Overview'});
         const container = contentEl.createDiv({cls: 'yearContainer'});
-        for(let i = 0; i < 12; i++) {
-            container.createDiv({cls:"monthContainer"});
+        for (let i = 0; i < 12; i++) {
+            const month = container.createDiv({cls:"monthContainer"});
+            month.createEl("h4", {text:MONTHS[i], cls:"monthName"})
         }
     }
 }
