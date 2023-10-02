832b47f3ada0ee61ec41006d6e8e74806a8ff512
Raboro <marius.woerfelreal@gmail.com>
2023-10-02 17:34:30 +0200

    added border to month

--- src/views/yearOverviewView.ts
+++ src/views/yearOverviewView.ts
@@ -18,7 +18,7 @@ export class YearOverviewView extends ItemView {
         contentEl.createEl('h1', {text: 'Birthday Tracker - Year Overview'});
         const container = contentEl.createDiv({cls: 'yearContainer'});
         for(let i = 0; i < 12; i++) {
-            container.createDiv({text:""});
+            container.createDiv({cls:"monthContainer"});
         }
     }
 }
