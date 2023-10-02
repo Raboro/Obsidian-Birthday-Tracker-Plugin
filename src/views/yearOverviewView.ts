c29ec57d328e2eff013ce6d8b6786a91eaf65b03
Raboro <marius.woerfelreal@gmail.com>
2023-10-02 17:03:04 +0200

    added icon

--- src/views/yearOverviewView.ts
+++ src/views/yearOverviewView.ts
@@ -3,7 +3,8 @@ import { ItemView } from "obsidian";
 export const BIRTHDAY_TRACKER_YEAR_OVERVIEW_VIEW_TYPE = "Birthday-Tracker-Year-Overview";
 
 export class YearOverviewView extends ItemView {
-    
+    icon = "calendar-days";
+
     getViewType(): string {
         return BIRTHDAY_TRACKER_YEAR_OVERVIEW_VIEW_TYPE;
     }
