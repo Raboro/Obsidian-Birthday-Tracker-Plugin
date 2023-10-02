346d49e5120d8c35da458bcf7a0a91e9b0948ae0
Raboro <marius.woerfelreal@gmail.com>
2023-10-02 16:41:06 +0200

    added basic view

---
+++ src/views/yearOverviewView.ts
@@ -0,0 +1,19 @@
+import { ItemView } from "obsidian";
+
+export const BIRTHDAY_TRACKER_YEAR_OVERVIEW_VIEW_TYPE = "Birthday-Tracker-Year-Overview";
+
+export class YearOverviewView extends ItemView {
+    
+    getViewType(): string {
+        return BIRTHDAY_TRACKER_YEAR_OVERVIEW_VIEW_TYPE;
+    }
+
+    getDisplayText(): string {
+        return BIRTHDAY_TRACKER_YEAR_OVERVIEW_VIEW_TYPE;
+    }
+
+    async onOpen() {
+        const { contentEl } = this;
+        contentEl.createEl('h1', {text: 'Birthday Tracker - Year Overview'});
+    }
+}
