76de55851e6b527b82266dfb319ef90b4ec2401e
Raboro <marius.woerfelreal@gmail.com>
2024-09-18 22:26:30 +0200

    refactor(GHActionbot): :art: formatted & linting & organized imports with biome

--- scripts/linter.sh
+++ scripts/linter.sh
@@ -2,7 +2,7 @@
 
 echo -e "\ntry to fix all issues if present"
 
-bun run bione:write
+bun run biome:write
 
 git commit -am "refactor(GHActionbot): :art: formatted & linting & organized imports with biome"
 
